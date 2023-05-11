6bcdf7a32e7f8856abdaea3f08d90edb1e61d4be
Yago Maksuell Lacerda Cardoso <yago.cardoso@linx.com.br>
2023-05-11 15:30:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const input = document.querySelector("input");
+const qrcode = document.querySelector("#qrcode");
+
+
+document.addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+        geraQrcode();
+    }
+});
+
+
+function geraQrcode(){
+    if(!input.value) return;
+
+    qrcode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+}
